f79589e6dd53efa90d21aecb4a53d36ee8d5fa43
Alex Barry <aostreetart9@gmail.com>
2018-04-02 22:41:25 -0400

    correct module name in addon test

--- tests/addon/addon.test.py
+++ tests/addon/addon.test.py
@@ -4,7 +4,7 @@ import sys
 
 # Install and Enable the Addon within the currently running blender instance
 bpy.ops.wm.addon_install(filepath="/home/travis/build/AO-StreetArt/BlenderSync/blendersync.py")
-bpy.ops.wm.addon_enable(module="BlenderSync")
+bpy.ops.wm.addon_enable(module="blendersync")
 
 #Import the addon for the purposes of the test script
 import blendersync
