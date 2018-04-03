db5e33ef419ebfa80abffd9b7f283405a649f03f
Alex Barry <aostreetart9@gmail.com>
2018-04-02 23:47:05 -0400

    we don't need to manually install add-on in blender test python file, as it's already symlinked to the right path

--- tests/addon/addon.test.py
+++ tests/addon/addon.test.py
@@ -3,7 +3,7 @@ import bpy
 import sys
 
 # Install and Enable the Addon within the currently running blender instance
-bpy.ops.wm.addon_install(filepath="/home/travis/build/AO-StreetArt/BlenderSync/blendersync.py")
+# bpy.ops.wm.addon_install(filepath="/home/travis/build/AO-StreetArt/BlenderSync/blendersync.py")
 bpy.ops.wm.addon_enable(module="blendersync")
 
 #Import the addon for the purposes of the test script
