2130f7662c5f8896d5f7aead62c4376774fcc452
Alex Barry <aostreetart9@gmail.com>
2018-04-02 22:00:29 -0400

    fix folder paths in travis CI

--- tests/addon/addon.test.py
+++ tests/addon/addon.test.py
@@ -4,7 +4,7 @@ import bpy
 homeDirectory = sys.argv[1]
 
 # Install and Enable the Addon within the currently running blender instance
-bpy.ops.wm.addon_install(filepath=homeDirectory + "blendersync/blendersync.py")
+bpy.ops.wm.addon_install(filepath=homeDirectory + "BlenderSync/blendersync.py")
 bpy.ops.wm.addon_enable(module="BlenderSync")
 
 #Import the addon for the purposes of the test script
